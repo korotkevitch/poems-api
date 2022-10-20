d47d8640b960fbe461058c782991aad9293c79dd
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-10-20 16:10:34 +0300

    Created models

--- poem/models.py
+++ poem/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Holiday(models.Model):
+    name = models.CharField('Праздник', max_length=30)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class META:
+        verbose_name = 'Праздник'
+        verbose_name_plural = 'Праздники'
+
+
+class Author(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField('Фамилия', max_length=20)
+    slug = models.SlugField(max_length=110, unique=True, blank=True)
+
+    def get_url(self):
+        return reverse('single_holiday', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.last_name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+
+class Poem(models.Model):
+    holiday = models.ForeignKey(Author, verbose_name="Праздник", on_delete=models.CASCADE)
+    text = models.TextField('Текст')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(verbose_name='Опубликовано', default=True)
+    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.holiday
+
+    class META:
+        verbose_name = 'Стихи'
+        verbose_name_plural = 'Стихи'
+
+
+
