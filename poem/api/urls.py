e28060f896b76d1858bcf733f71f14632c95ca9d
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-10-22 16:11:22 +0300

    Utilized djoser for registration

--- poem/api/urls.py
+++ poem/api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('api/poem/<int:pk>/', PoemAPIUpdate.as_view()),
     path('api/poemdelete/<int:pk>/', PoemAPIDestroy.as_view()),
 
-    # re_path(r'^auth/', include('djoser.urls')),
-    # re_path(r'^auth/', include('djoser.urls.jwt')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
