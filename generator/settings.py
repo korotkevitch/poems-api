3e4aa7a129d06b71660672ba6ae1735c00dd5d60
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-10-27 19:34:27 +0300

    Added pagination in settings

--- generator/settings.py
+++ generator/settings.py
@@ -162,8 +162,8 @@ REST_FRAMEWORK = {
         'review-detail': '100/day',
     },
 
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 5,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
 
     # 'DEFAULT_RENDERER_CLASSES': (
     #     'rest_framework.renderers.JSONRenderer',
