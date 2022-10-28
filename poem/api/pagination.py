af08fe91aba8b3d4c351a92bc8eb779f01cebf06
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-10-28 10:33:37 +0300

    Edited pagination

--- poem/api/pagination.py
+++ poem/api/pagination.py
@@ -3,20 +3,20 @@ from rest_framework.pagination import PageNumberPagination, LimitOffsetPaginatio
 
 class PoemPagination(PageNumberPagination):
     page_size = 3
-    page_query_param = 'p' # вторая страница будет /?p=2
+    # page_query_param = 'p' # вторая страница будет /?p=2
     page_size_query_param = 'size'  # чтобы изменить кол-во записей на стр. http://127.0.0.1:8000/api/poem/?p=2&size=3
     max_page_size = 10
     # last_page_strings = 'end'
 
 
-class PoemLOPagination(LimitOffsetPagination):
-    default_limit = 5
-    max_limit = 10
-    limit_query_param = 'limit'
-    offset_query_param = 'start'
-
-
-class PoemCPagination(CursorPagination):
-    page_size = 5
-    ordering = 'created'
-    cursor_query_param = 'record'
+# class PoemLOPagination(LimitOffsetPagination):
+#     default_limit = 5
+#     max_limit = 10
+#     limit_query_param = 'limit'
+#     offset_query_param = 'start'
+#
+#
+# class PoemCPagination(CursorPagination):
+#     page_size = 5
+#     ordering = 'created'
+#     cursor_query_param = 'record'
