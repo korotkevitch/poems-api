41a1a912871481b410bb39f62fdeba6023def305
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-10-29 22:30:06 +0300

    test_api_jwt works fine

--- poem/api/views.py
+++ poem/api/views.py
@@ -25,7 +25,7 @@ class HolidayAPIList(generics.ListCreateAPIView):
 class PoemAPIList(generics.ListCreateAPIView):
     queryset = Poem.objects.all()
     serializer_class = PoemSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
     pagination_class = PoemPagination
 
 
--- poem/tests/test_api.py
+++ poem/tests/test_api.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 from poem.models import Holiday
 from poem.api.serializers import HolidaySerializer
 
@@ -21,28 +22,20 @@ class HolidayApiTestCase(APITestCase):
 class TokenAuthUserProfileTestCase(APITestCase):
     def test_api_jwt(self):
         url = reverse('token_obtain_pair')
-        u = User.objects.create_user(username='iko9', password='i041291ko')
-        u.is_active = True
-        u.save()
+        user = User.objects.create_user(username='iko9', password='i041291ko')
+        user.is_active = True
+        user.save()
+        client = APIClient()
 
         response = self.client.post(url, {'username': 'iko9', 'password': 'i041291ko'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # print(response.data)
         self.assertTrue('access' in response.data)
-        access = response.data['access']
-        print(access)
+        token = response.data['access']
 
-        verification_url = 127.0.0.1:8000/auth/jwt/verify/
-        response = self.client.post(verification_url, {'token': access}, format='json')
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + 'abc')
+        response = client.get('/api/poem/', data={'format': 'json'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        response = client.get('/api/poem/', data={'format': 'json'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # resp = self.client.post(verification_url, {'token': 'abc'}, format='json')
-        # self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
-        #
-        # client = APIClient()
-        # client.credentials(HTTP_AUTHORIZATION='JWT ' + 'abc')
-        # resp = client.get('/api/v1/account/', data={'format': 'json'})
-        # self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
-        # client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
-        # resp = client.get('/api/v1/account/', data={'format': 'json'})
-        # self.assertEqual(resp.status_code, status.HTTP_200_OK)
