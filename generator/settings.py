c9bba98d75ad59bbd18cb4c6ca2be7662a241350
Igor Korotkevitch <i.korotkevitch@gmail.com>
2022-11-06 21:57:18 +0300

    Removed content from dbdump.sql

--- generator/settings.py
+++ generator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'poems.korotkevitch.online']
+ALLOWED_HOSTS = ['127.0.0.1', 'shop.korotkevitch.online']
 
 
 # Application definition
